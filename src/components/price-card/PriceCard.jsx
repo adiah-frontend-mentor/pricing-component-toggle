65c7eb14e0fa7f22edc4160eb279a4999ba280fb
nathanieladiah <nathanieladiah@gmail.com>
2022-05-13 22:07:32 -0400

    Add react-transition-group for pricing transitions

--- src/components/price-card/PriceCard.jsx
+++ src/components/price-card/PriceCard.jsx
@@ -1,13 +1,26 @@
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import './price-card.scss';
 
 const PriceCard = ({data, pricing}) => {
   return (
 	<div className={`price-card ${data.promoted ? 'price-card--promoted' : ''}`}>
 		<h2 className='price-card__heading'>{data.type}</h2>
+
 		<div className="price-card__price-wrapper">
 			<span className="price-card__dollars">$</span>
-			<p className="price-card__price">{pricing === 'annual' ? data.price.annually : data.price.monthly}</p>
+			<SwitchTransition>
+				<CSSTransition
+					key={pricing}
+					timeout={400}
+					classNames={'price-card__price-'}
+				>
+					<p className="price-card__price">{pricing === 'annual' ? data.price.annually : data.price.monthly}</p>
+				</CSSTransition>
+			</SwitchTransition>
+
 		</div>
+
+
 		<ul className="price-card__features-list">
 			<li className="price-card__feature">{data.storage.capacity} {data.storage.units} Storage</li>
 			<li className="price-card__feature">{data.users} Users Allowed</li>
